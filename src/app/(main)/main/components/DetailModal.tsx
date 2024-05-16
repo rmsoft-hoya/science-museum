084f47bceee12ca55105b64d8e2e618610c1ab43
hoya <152576347+rmsoft-hoya@users.noreply.github.com>
2024-05-17 08:37:18 +0900

    fix: 측우기 측우대 설명 변경

--- src/app/(main)/main/components/DetailModal.tsx
+++ src/app/(main)/main/components/DetailModal.tsx
@@ -13,48 +13,44 @@ const DetailModal = () => {
   return (
     <AlertDialog open={isModalOpen}>
       <AlertDialogContent className="bg-[#152E64] flex h-full border-0 outline-none max-h-[684px] w-[326px] flex-col rounded-[16px] py-10 px-2 text-white">
-        <div className="absolute top-[16px] right-[16px]">
-          <X size={24} strokeWidth={2} className="cursor-pointer" onClick={() => onClose()} />
-        </div>
         {/* 제목 */}
         <AlertDialogHeader className="flex">
           <div className="flex w-full items-center justify-center">
-            <AlertDialogTitle className="text-2xl font-bold">측우기와 측우대</AlertDialogTitle>
+            <AlertDialogTitle className="text-xl font-bold">측우기(測雨器)와 측우대(測雨臺)</AlertDialogTitle>
           </div>
         </AlertDialogHeader>
         {/* 내용 */}
-        <div className="overflow-y-auto px-4 h-full">
-          <div className="flex flex-col gap-8">
+        <div className="overflow-y-auto px-4">
+          <div className="flex flex-col gap-6">
             <div className="flex flex-col gap-2">
-              <h2 className="text-base font-semibold">통영측우대(과학기술사-4-2019)</h2>
+              <h2 className="text-base font-semibold">개요</h2>
               <p className="leading-[140%] text-sm">
-                <p className="indent-1">
-                  측우대는 강우량을 측정하는 기기인 측우기를 안정되게 얹히기 위 한 받침돌로 윗면이 오목하게 파인 사각기둥 형태를 가지고 있다. <br />
-                  <br />
-                </p>
-                <p className="indent-1">
-                  통영측우대는 19세기 경남 통영에 위치한 수군 통제영에서 설치했 던 것으로 1910년경 관측소(현 기상청)로 옮겨서 보관되었다.
-                  <br />
-                  <br />
-                </p>
-                <p className="indent-1">
-                  이후 1972년부터는 서울과학관에서, 그리고 2008년부터는 국립중앙과 학관에서 전시되고 있다.
-                  <br />
-                  <br />
-                </p>
-                <p className="indent-1">
-                  통영측우대는 현존하는 측우대 중 유일하게 받침대를 갖추고 있으 며, ‘測雨臺(측우대)’라는 명칭과 ‘辛未 二月(신미 2월)’이라고 제작 연대가 명확하게
-                  새겨져 있다.
-                  <br />
-                  <br />
-                </p>
-                <p className="indent-1">
-                  이는 조선시대 전국 기상 관측망의 존재를 입증하는 중요한 증거로서, 우리나라가 세계에서 가장 오랜 기간의 강우량 기록을 보유한 국가임을 확인하는
-                  중요한 자료이다.
-                  <br />
-                  <br />
-                </p>
-                <p className="indent-1">이러한 역사적 가치를 인정받아 2010년에 보물 제1652호로 지정되 었다.</p>
+                측우기는 1441년(세종23년)에 왕세자였던 문종에 의해 발명된 조선시대 우량 측정기구이다. 이 측우기의 받침대가 측우대이 다. 15세기 비의 양을
+                인공적인 기구를 이용하여 정밀한 수치 로 측정한 것은 세계 과학사에 있어서 획기적인 일이었다.
+              </p>
+            </div>
+            <div className="flex flex-col gap-2">
+              <h2 className="text-base font-semibold">설명</h2>
+              <p className="leading-[140%] text-sm indent-3">
+                조선은 농업국가로, 농업생산량의 증산에 필요한 농업용수 확보 를 위해 강우량 측정이 중요하였다. 그러나 강우량 측정기구가 없어 비 온 뒤 땅에 비가
+                스민 깊이를 재었으나 땅이 빗물에 스 민 깊이는 토양의 습도에 따라 달라지므로 빗물의 양을 정확히 알기 어려웠다.
+              </p>
+              <p className="leading-[140%] text-sm indent-3">
+                이에 세종실록에 의하면 문종은 왕세자 시절에 강우량 측정법을 연구하여 세계 최초 강우량계인 측우기를 발명하였다. 측우기 는 주철로 된 원통형
+                그릇으로, 높이 1척 5촌(약 32cm), 지름 7 촌(약 15cm)이며, 돌로 만든 측우대 위에 올려놓고 비가 그친 후 주척(周尺)을 써서 푼(分) 단위까지 재고,
+                비가 내리기 시작 한 시간과 그친 시간을 측정하도록 하였다.
+              </p>
+              <p className="leading-[140%] text-sm indent-3">
+                현존하는 측우기는 1837년 공주 충청감영에 설치되었던 금영측 우기가 유일하며, 측우대는 대구 경상감영 측우대, 창덕궁 이 문원 측우대, 관상감 측우대,
+                통영 측우대 등 4기만 남아 있다.
+              </p>
+              <p className="leading-[140%] text-sm indent-3">
+                그 중 통영 측우대(보물)는 현존하는 측우대 중 유일하게 받침대 를 갖추고 있으며, ‘測雨臺(측우대)’와 ‘辛未二月(신미 2월)’이라 는 명문이 있어 명칭과
+                제작 연대도 알 수 있다.
+              </p>
+              <p className="leading-[140%] text-sm indent-3">
+                측우기와 측우대는 조선시대 기상 관측을 입증하는 중요한 증거 로, 우리나라가 세계에서 가장 오랜 기간 강우량 측정 및 기록 을 보유한 국가임을 알 수
+                있는 중요한 자료이다.
               </p>
             </div>
           </div>
